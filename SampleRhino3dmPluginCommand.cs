0d194ae159d59e33e4b8351da67f696916cbb3b5
fraguada <fraguada@gmail.com>
2024-02-26 21:02:25 +0100

    first

---
+++ SampleRhino3dmPluginCommand.cs
@@ -0,0 +1,77 @@
+﻿extern alias Rhino3dm;
+using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace SampleRhino3dmPlugin
+{
+    public class SampleRhino3dmPluginCommand : Command
+    {
+        public SampleRhino3dmPluginCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static SampleRhino3dmPluginCommand Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "SampleRhino3dmPluginCommand";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: start here modifying the behaviour of your command.
+            // ---
+            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
+
+            Point3d pt0;
+            using (GetPoint getPointAction = new GetPoint())
+            {
+                getPointAction.SetCommandPrompt("Please select the start point");
+                if (getPointAction.Get() != GetResult.Point)
+                {
+                    RhinoApp.WriteLine("No start point was selected.");
+                    return getPointAction.CommandResult();
+                }
+                pt0 = getPointAction.Point();
+            }
+
+            Point3d pt1;
+            using (GetPoint getPointAction = new GetPoint())
+            {
+                getPointAction.SetCommandPrompt("Please select the end point");
+                getPointAction.SetBasePoint(pt0, true);
+                getPointAction.DynamicDraw +=
+                  (sender, e) => e.Display.DrawLine(pt0, e.CurrentPoint, System.Drawing.Color.DarkRed);
+                if (getPointAction.Get() != GetResult.Point)
+                {
+                    RhinoApp.WriteLine("No end point was selected.");
+                    return getPointAction.CommandResult();
+                }
+                pt1 = getPointAction.Point();
+            }
+
+            doc.Objects.AddLine(pt0, pt1);
+            doc.Views.Redraw();
+            RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
+            
+            var file3dm = new Rhino3dm.Rhino.FileIO.File3dm();
+            var pt0_3dm = new Rhino3dm.Rhino.Geometry.Point3d(pt0.X, pt0.Y, pt0.Z);
+            var pt1_3dm = new Rhino3dm.Rhino.Geometry.Point3d(pt1.X, pt0.Y, pt0.Z);
+            file3dm.Objects.AddLine(pt0_3dm, pt1_3dm, null);
+            file3dm.Write("file3dm.3dm", 8);
+            
+
+            // ---
+            return Result.Success;
+        }
+
+        
+    }
+}
